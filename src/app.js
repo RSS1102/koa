0ad4dd6a668dc1766a2ae91b9b916f26feb93f34
任帅 <oncwnuJDerqc501ieO3t9lujAGrg@git.weixin.qq.com>
2021-12-13 04:13:10 +0800

    开发微信扫码登陆

--- src/app.js
+++ src/app.js
@@ -1,15 +1,19 @@
 const koa = require('koa')
+const koabody = require('koa-bodyparser')
 const Router = require('koa-router')
 const app = new koa()
 const routers = require("./router/index.route")
-    //注册
-app.use(routers.routes())
 
-// app.use((res, req) => {
-//     res.body = "来到后端"
-// })
+//注册
+/* 
+注意这里：
+1,是：koa-bodyparser，不是koa-body
+2,koabody()要写在routers.routes()之前
+*/
+app.use(koabody()).use(routers.routes())
 
-const post = 3003
+
+const post = 8080
 app.listen(post, () => {
     console.log("serve:", `http://localhost:${post}`)
 })
--- src/router/index.route.js
+++ src/router/index.route.js
@@ -1,10 +1,31 @@
 const Router = require('koa-router')
-const router = new Router({ prefix: "/" })
+const router = new Router({ prefix: "/api" })
+const https = require('https')
+const wxlogin = {
+    APPID: "wx0a7924b7c9a2e3b6",
+    scope: "snsapi_login",
+    redirect_uri: "http://www.cokbbs.games/index",
+}
 
-router.get('/', (res, req) => {
-    res.body = "koa"
+router.get('/wxlogin', (res, req) => {
+
+    const wx_href = `https://open.weixin.qq.com/connect/qrconnect?appid=${wxlogin.APPID}&redirect_uri=${wxlogin.redirect_uri}&response_type=code&scope=${wxlogin.scope}&state=STATE# wechat_redirect`;
+    res.body = wx_href;
+
+
 })
+router.post('/wxcode', async(ctx, next) => {
+    // 获取access_token
+    console.log(ctx.request.body.urlSearchParams)
+    let Code = '071LhPkl2MChh84ecrml2vxIE33LhPkB'
+    let SECRET = '1c63850cadf79db3bf0f1325e88136c3'
+    const token = https.get(`https://api.weixin.qq.com/sns/oauth2/access_token?appid=${wxlogin.APPID}&secret=${SECRET}&code=${Code}&grant_type=authorization_code`)
+    console.log(token)
 
+
+
+    ctx.body = "3"
+})
 
 
 module.exports = router
