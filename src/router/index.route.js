3ca5dcc1bb9c8b33eb0301aec8567e2a752c1471
任帅 <oncwnuJDerqc501ieO3t9lujAGrg@git.weixin.qq.com>
2021-12-14 21:45:54 +0800

    完成微信扫码授权登陆

--- src/router/index.route.js
+++ src/router/index.route.js
@@ -1,10 +1,11 @@
 const Router = require('koa-router')
 const router = new Router({ prefix: "/api" })
 const https = require('https')
+const urlencode = require('urlencode');
 const wxlogin = {
     APPID: "wx0a7924b7c9a2e3b6",
     scope: "snsapi_login",
-    redirect_uri: "http://www.cokbbs.games/index",
+    redirect_uri: urlencode.decode("http://www.cokbbs.games/index"),
 }
 
 router.get('/wxlogin', (res, req) => {
@@ -17,10 +18,34 @@ router.get('/wxlogin', (res, req) => {
 router.post('/wxcode', async(ctx, next) => {
     // 获取access_token
     console.log(ctx.request.body.urlSearchParams)
-    let Code = '071LhPkl2MChh84ecrml2vxIE33LhPkB'
+    let Code = ctx.request.body.urlSearchParams
     let SECRET = '1c63850cadf79db3bf0f1325e88136c3'
-    const token = https.get(`https://api.weixin.qq.com/sns/oauth2/access_token?appid=${wxlogin.APPID}&secret=${SECRET}&code=${Code}&grant_type=authorization_code`)
-    console.log(token)
+    https.get(`https://api.weixin.qq.com/sns/oauth2/access_token?appid=${wxlogin.APPID}&secret=${SECRET}&code=${Code}&grant_type=authorization_code`, (res => {
+
+        console.log("--------------------------------------------------")
+        res.on('data', (d) => {
+            // json
+            let token = JSON.parse(d)
+            console.log("token", token)
+            let ACCESS_TOKEN = token.access_token
+            let OPENDID = token.openid
+            console.log(ACCESS_TOKEN, OPENDID)
+            https.get(`https://api.weixin.qq.com/sns/userinfo?access_token=${ACCESS_TOKEN}&openid=${OPENDID}`, (res => {
+                res.on('data', (d) => {
+                    let userinfo = JSON.parse(d)
+                    console.log(userinfo)
+                    res.body = userinfo
+                        //应该储存在数据库内
+                })
+
+            }))
+
+        });
+
+    })).on('error', (e) => {
+        console.error(e);
+    })
+
 
 
 
